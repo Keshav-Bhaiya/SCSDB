04e960d157b6557eaa14146b330cfff4b3e03708
My <keshavb266@gmail.com>
2025-07-17 15:42:22 +0530

    new commits

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import loader from "/loader.gif";
 const Loading = ()=>{
   return(
     <div className="w-screen h-screen flex justify-center items-center bg-black">
-      <img className=" h-[80%] object-cover" src={loader} alt="" />
+      <img className=" h-[50%] object-cover" src={loader} alt="" />
       
     </div>
   );
